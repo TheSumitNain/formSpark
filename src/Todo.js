c1fcc06b0f77a659c9b7f53026682fecd0911f46
Sumit <namisumit740@gmail.com>
2022-02-16 16:49:21 +0530

    weather detail with searching city name

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Style.css";
+
+const Todo = () => {
+    return (
+        <>
+            <div className="main-div">
+
+            </div>
+        </>
+    )
+}
+
+export default Todo; 
